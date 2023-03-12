aeb941a6650783d4b720a2577cde856e91da594e
FLIKSS <104595546+FLIKSSCOD@users.noreply.github.com>
2023-03-12 14:07:48 +0300

    Add files via upload

---
+++ EX10/main.js
@@ -0,0 +1,56 @@
+var password = prompt('Enter your passwort');
+console.log(password);
+alert('Hahahaha, your passwort very esay');
+alert('Come up with a better password');
+
+var password1 = prompt('Enter your passwort');
+console.log(password1);
+alert('Try somethinng harder');
+
+var password2= prompt('Enter your passwort');
+console.log(password2);
+alert(password2);
+console.log('Confired password.');
+
+setTimeout(name1, 1)
+
+function name1() {
+	console.log('Confired password..')
+}
+
+setTimeout(name2, 2)
+
+function name2() {
+	console.log('Confired password...')
+}
+
+setTimeout(name3, 3)
+
+function name3() {
+	alert('You have confired by sending the password to me, Thanks')
+}
+
+setTimeout(name4, 4)
+
+function name4() {
+	console.log('Thanks')
+}
+
+setTimeout(name5, 5)
+
+function name5() {
+	alert('"Bay"')
+}
+
+setTimeout(name6, 6)
+
+function name6() {
+	console.log(password, password1, password2)
+}
+
+setTimeout(name7, 7)
+
+function name7() {
+	console.log('save and send')
+}
+
